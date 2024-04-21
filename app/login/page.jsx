cef3290e351a5bc1502853eead05e9964e208bbf
Tirth chokshi <chokshitirth4@gmail.com>
2024-04-21 20:33:11 +0530

    login crash fix

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,13 +1,11 @@
 import styles from "@/app/ui/login/login.module.css";
 import LoginForm from "../ui/login/loginForm/loginForm";
-import { login } from "@/components/login";
 const LoginPage = () => {
   return (
     <>
-      {/* <div className={styles.container}>
+      <div className={styles.container}>
         <LoginForm />
-      </div> */}
-      <login/>
+      </div>
     </>
   );
 };
